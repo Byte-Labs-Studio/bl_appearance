bf30d4217270549ec95b1a7eccf72cb98584b0b6
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-07-28 23:49:42 +0530

    disable ui dev mode

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,8 +8,8 @@ version '1.0.0'
 description 'Customize your virtual persona with precision using the Byte Labs Appearance Menu'
 repository 'https://github.com/Byte-Labs-Studio/bl_appearance'
 
--- ui_page 'build/index.html'
-ui_page 'http://localhost:3001/' --for dev
+ui_page 'build/index.html'
+--ui_page 'http://localhost:3001/' --for dev
 
 server_script 'dist/server/**/*.js'
 
