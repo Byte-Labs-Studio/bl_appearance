2e990bccfdfd6206cb887d9ffa6dc891609f8b0c
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-07-30 22:36:08 +0530

    Update appearance.sql

--- appearance.sql
+++ appearance.sql
@@ -9,9 +9,12 @@ CREATE TABLE IF NOT EXISTS `appearance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `outfits` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `player_id` varchar(100) NOT NULL,
-    `label` varchar(100) NOT NULL,
-    `outfit` longtext DEFAULT NULL,
-    PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player_id` varchar(100) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `outfit` longtext DEFAULT NULL,
+  `jobname` varchar(50) DEFAULT NULL,
+  `jobrank` int(2) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
