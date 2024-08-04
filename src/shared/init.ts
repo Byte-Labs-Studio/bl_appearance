ada6d88a09d2d4c9ce646dd68ce850ab04817840
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-08-04 21:06:24 +0530

    working dependency checker

--- src/shared/init.ts
+++ src/shared/init.ts
@@ -1,2 +1,31 @@
-if (!exports.ox_lib.checkDependency('bl_bridge', '1.2.5'))
-    console.error('You need to update your bridge resource to >= 1.2.5');
+function checkDependency(resource: string, version: string) {
+    let currentVersion = GetResourceMetadata(resource, 'version', 0)
+    currentVersion = currentVersion && currentVersion?.match(/\d+\.\d+\.\d+/)?.[0] || 'unknown'
+
+	if (currentVersion != version) {
+		const cv = currentVersion.split('.');
+        const mv = version.split('.');
+		const msg = `^1${GetInvokingResource() || GetCurrentResourceName()} requires version '${version}' of '${resource}' (current version: ${currentVersion})^0`;
+        for (let i = 0; i < cv.length; i++) {
+            const current = Number(cv[i]);
+            const minimum = Number(mv[i]);
+
+            if (current !== minimum) {
+                if (isNaN(current) || current < minimum) {
+                    console.error(msg);
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+checkDependency('bl_bridge', '1.2.5');
+
+const HideHud = (state?: boolean) => {
+    if (state==null) return;
+}
+
+HideHud();
