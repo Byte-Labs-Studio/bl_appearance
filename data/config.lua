53ddee25c6ad74e8c1ae50985977d084a62bca73
Xirvin <xander228irvin@gmail.com>
2024-08-05 16:13:16 +1000

    chore(hud): qb init

--- data/config.lua
+++ data/config.lua
@@ -11,4 +11,9 @@ end)
 ---@param state boolean If true, hides the HUD. If false, shows the HUD.
 exports('hideHud', function(state)
     -- Implement your code here
+    local qbhud = GetResourceState('qb-hud') == 'started'
+    if qbhud then
+        -- qb hud is trash and doesnt have a hide function
+        DisplayRadar(state)
+    end
 end)
