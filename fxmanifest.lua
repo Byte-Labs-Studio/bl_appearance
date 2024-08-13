e14f26f46a87e5ca0f15f568c93f6b0ee0ea0ed6
BL Development <SOH69@users.noreply.github.com>
2024-08-13 07:34:03 +0000

    chore: bump manifest version to v1.1.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game "gta5"
 lua54 'yes'
 
 author "Byte Labs"
-version '1.1.1'
+version '1.1.2'
 description 'Customize your virtual persona with precision using the Byte Labs Appearance Menu'
 repository 'https://github.com/Byte-Labs-Studio/bl_appearance'
 
